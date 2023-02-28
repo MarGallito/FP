8a030bb7ca6295838b3888d4ce66debe919dc911
MarGallito <123398118+MarGallito@users.noreply.github.com>
2023-02-28 23:56:40 +0400

    script add

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+// smoothly scroll
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach(anchor => {
+	anchor.addEventListener('click', event => {
+		event.preventDefault();
+
+		const blockID = anchor.getAttribute('href').substring(1);
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
+	})
+})
